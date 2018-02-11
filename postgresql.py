69e5202e8661522ea775e48f3998bd65624f4bc9
Erik <eirikval@gmail.com>
2018-02-11 14:39:33 -0800

    Adding NumPy style docstrings and started refactoring

--- postgresql.py
+++ postgresql.py
@@ -1,5 +1,5 @@
 """
-Connecting to PostgreSQL
+This module contains the
 """
 
 from sqlalchemy import create_engine
@@ -8,31 +8,48 @@ from sqlalchemy.orm import sessionmaker
 
 class PostgreSqlHandle:
 
-    """
-    Handler to a PostgreSQL database and its contents
-    """
-
     def __init__(self, db_name, username,
                  password, hostname, port=5432):
         """
         Initializes SQLAlchemy engine, using psycopg2 as the DBAPI
-
         http://docs.sqlalchemy.org/en/latest/dialects/postgresql.html#dialect-postgresql-psycopg2-connect
 
-        :param db_name: <str>
-        :param username: <str>
-        :param password: <str>
-        :param hostname: <str>
-        :param port: <int>
+        Parameters
+        ----------
+        db_name : str
+            The target PostgreSQL database name
+        username : str
+            Username credential for the PostgreSQL database
+        password : str
+            Password credential for the PostgreSQL database
+        hostname : str
+            The PostgreSQL instance name, ex. 'localhost'
+        port : int
+            The port that the target PostgreSQL instance is
+            running on, default is 5432
+
+        Attributes
+        ----------
+        conn_str: str
+            The psycopg2 connection string, formatted with
+            the instance's connection params
+        engine: SQLAlchemy engine
+            Initialized with the psycopg2 formatted conn_str
+        Session: SQLAlchemy Session() class
+            A class created from the sessionmaker class factory,
+            with the current engine bound, so new instances of
+            the Session() class can be constructed if needed using
+            the current connection.
         """
-        self.connection_string = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(
+
+        self.conn_str = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'.format(
             username,
             password,
             hostname,
             port,
             db_name
         )
-        self.engine = create_engine(self.connection_string)
+        self.engine = create_engine(self.conn_str)
         self.Session = sessionmaker(bind=self.engine)
 
         # Check that the database connection works
@@ -47,35 +64,47 @@ class PostgreSqlHandle:
             raise
 
 
-    def bulk_copy(self, sql_schema_and_table, csv_file_path,
-                  local_to_remote=False):
+    def bulk_copy(self, csv_file_path, sql_schema,
+                  sql_table, local_to_remote=False):
         """
-        Attempts to load a CSV file into a destination SQL table using the the current
-        SQLAlchemy engine's raw_connection() method.
+        Attempts to load a CSV file into a destination SQL table
+        using the PostgreSQL native COPY functionality, which is
+        an optimized way of loading large data sets.
+        https://www.postgresql.org/docs/current/static/sql-copy.html
 
-        :param sql_schema_and_table: <str> in the form
-            <destination_schema_name>.<destination_table_name>
-        :param csv_file_path: <str> path of csv file to load to SQL
-        :param local_to_remote: <bool> Defaults to False, meaning the SQL database
-            is on the system running this client code. Otherwise set to true, i.e. SQL
-            database is being accessed remotely.
+        Notes
+        -----
+        Assumes the CSV file has a header and the destination SQL
+        tables already exist, so the header of the CSV is skipped.
 
-        https://www.postgresql.org/docs/9.2/static/sql-copy.html
+        Parameters
+        ----------
+        csv_file_path : str
+            The file path for the CSV file to load
+        sql_schema : str
+            The SQL schema/namespace of the SQL table
+            to load the CSV file into
+        sql_table : str
+            The name of the SQL table to load the CSV file
+            into
+        local_to_remote : bool
+            Default False, uses the COPY method, if True,
+            then uses copy_expert of pyscopg2 to alter
+        Returns
+        -------
 
-        :return:
         """
+
         dbapi_conn = self.engine.raw_connection()
         cursor = dbapi_conn.cursor()
 
         try:
             if not local_to_remote:
-                sql = ("COPY {0} FROM '{1}' WITH CSV HEADER".
-                       format(sql_schema_and_table, csv_file_path))
-                cursor.execute(sql)
+                sql = "COPY %s.%s FROM %s WITH CSV HEADER"
+                cursor.execute(sql, (sql_schema, sql_table, csv_file_path))
                 dbapi_conn.commit()
             else:
-                sql = ("COPY {0} FROM STDIN WITH CSV HEADER".
-                       format(sql_schema_and_table))
+                sql = "COPY %s.%s FROM STDIN WITH CSV HEADER"
                 with open(csv_file_path) as file:
                     cursor.copy_expert(sql, file)
                 dbapi_conn.commit()
@@ -83,6 +112,7 @@ class PostgreSqlHandle:
             raise Exception
         finally:
             cursor.close()
+        return None
 
 
 
