f3ad261ef0aee116be794affb3cffb51895959b5
Erik Kvale <eirikval@gmail.com>
2018-07-20 23:47:28 -0500

    Got bulk functions working, will refactor and test next

--- dbhandlers/bulky.py
+++ dbhandlers/bulky.py
@@ -11,15 +11,6 @@ import sqlalchemy
 import pandas
 import numpy
 
-#================================
-# DataFrame
-#================================
-df = pandas.DataFrame(
-    data=numpy.random.randint(
-        0, 100,
-        size=(100000, 4)),
-    columns=list('ABCD')
-)
 
 #================================
 # To SQL
@@ -42,7 +33,6 @@ engine = sqlalchemy.create_engine(conn_str, use_batch_mode=True)
 dbapi_conn = engine.raw_connection()
 
 
-
 def _to_sql(csv_file, sql_table, conn):
     cursor = conn.cursor()
     table_composable = sql.Identifier(sql_table)
@@ -51,24 +41,32 @@ def _to_sql(csv_file, sql_table, conn):
     with open(csv_file, mode='r') as f:
         cursor.copy_expert(sql=sql_query, file=f)
         conn.commit()
+    return cursor.rowcount, cursor.statusmessage
 
-def load_csv():
+
+def load_csv(df):
     try:
         with NamedTemporaryFile(delete=False, suffix='.csv') as temp_csv:
             temp_csv.close()
-            df.to_csv(temp_csv.name, index=False, header=True)
-            _to_sql(temp_csv.name, 'test_data', dbapi_conn)
+            df.to_csv(temp_csv.name)
+            result = _to_sql(temp_csv.name, 'test_data', dbapi_conn)
     except:
         raise
     finally:
-        pass
-        # os.unlink(temp_csv.name)
+        os.unlink(temp_csv.name)
+    return result
 
 
 
 
 
 if __name__ == '__main__':
+    df = pandas.DataFrame(
+        data=numpy.random.randint(
+            0, 100,
+            size=(10005, 4)),
+        columns=['A', 'B', 'C', 'D']
+    )
     start = time.time()
     df.to_sql('test_data', engine, if_exists='append')
     end = time.time()
@@ -77,7 +75,8 @@ if __name__ == '__main__':
     print('-'*20)
 
     start = time.time()
-    load_csv()
+    result = load_csv(df)
     end = time.time()
-    print("DataFrame.to_sql() time: {}".format(end - start))
+    elapsed_time = end - start
+    print("psycopg2 copy_expert() time: {} Rows: {}".format(elapsed_time, result))
 
