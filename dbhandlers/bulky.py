133a8f576e9262eefe483801f82f07d2e2c5c1fc
Erik Kvale <eirikval@gmail.com>
2018-07-21 12:51:52 -0500

    Refactored bulky into class with 80 lines of code, derp!

--- dbhandlers/bulky.py
+++ dbhandlers/bulky.py
@@ -12,70 +12,93 @@ import pandas
 import numpy
 
 
-#================================
-# To SQL
-#================================
-_conn_dict = {
-    'dbname': 'test_pg',
-    'user': 'postgres',
-    'password': 'Gunnar14',
-    'host': 'localhost',
-    'port': 5432
-}
-conn_str = ('postgresql+psycopg2://'
-            '{user}:'
-            '{password}@'
-            '{host}:'
-            '{port}/'
-            '{dbname}'.format(**_conn_dict))
+class PostgreSqlHandler:
 
-engine = sqlalchemy.create_engine(conn_str, use_batch_mode=True)
-dbapi_conn = engine.raw_connection()
+    def __init__(self, **kwargs):
+
+        self._conn_dict = {
+            'dbname': '',
+            'user': '',
+            'password': '',
+            'host': 'localhost',
+            'port': 5432
+        }
+        for attr, value in kwargs.items():
+            if attr in self._conn_dict:
+                self._conn_dict[attr] = value
+
+        self._dsn_conn_str = (
+            "postgresql://"
+            "{user}:"
+            "{password}@"
+            "{host}:"
+            "{port}/"
+            "{dbname}".format(**self._conn_dict))
+
+        self.engine = sqlalchemy.create_engine(
+            self._dsn_conn_str, use_batch_mode=True
+        )
+        if self._check_engine(self.engine):
+            self._dbapi_conn = self.engine.raw_connection()
+
+    def dataframe_to_sql(self, dataframe, sql_table_name,
+                         **to_csv_kwargs):
+        try:
+            with NamedTemporaryFile(delete=False, suffix='.csv') as temp_csv:
+                temp_csv.close()
+                dataframe.to_csv(temp_csv, **to_csv_kwargs)
+                result = self._to_sql(
+                    temp_csv.name,
+                    sql_table_name,
+                    self._dbapi_conn
+                )
+        except:
+            raise
+        finally:
+            os.unlink(temp_csv.name)
+        return result
 
 
-def _to_sql(csv_file, sql_table, conn):
-    cursor = conn.cursor()
-    table_composable = sql.Identifier(sql_table)
-    sql_query = (sql.SQL("COPY {} FROM STDIN WITH CSV HEADER;").
-                 format(table_composable))
-    with open(csv_file, mode='r') as f:
-        cursor.copy_expert(sql=sql_query, file=f)
-        conn.commit()
-    return cursor.rowcount, cursor.statusmessage
+    def _to_sql(self, csv_file, table,
+                conn):
+        cursor = conn.cursor()
+        table_composable = sql.Identifier(table)
+        sql_query = (sql.SQL("COPY {} FROM STDIN WITH CSV HEADER;").
+                     format(table_composable))
+        with open(csv_file, mode='r') as f:
+            cursor.copy_expert(sql=sql_query, file=f)
+            conn.commit()
+        return (
+            cursor.rowcount,
+            cursor.statusmessage
+        )
 
-
-def load_csv(df):
-    try:
-        with NamedTemporaryFile(delete=False, suffix='.csv') as temp_csv:
-            temp_csv.close()
-            df.to_csv(temp_csv.name)
-            result = _to_sql(temp_csv.name, 'test_data', dbapi_conn)
-    except:
-        raise
-    finally:
-        os.unlink(temp_csv.name)
-    return result
-
-
-
-
+    def _check_engine(self, engine):
+        try:
+            conn = engine.connect()
+            conn.close()
+        except:
+            raise
+        return True
 
 if __name__ == '__main__':
+    pg_handler = PostgreSqlHandler(
+        dbname='test_pg',
+        user='postgres',
+        password='Gunnar14',
+        host='localhost',
+    )
     df = pandas.DataFrame(
         data=numpy.random.randint(
             0, 100,
             size=(10005, 4)),
         columns=['A', 'B', 'C', 'D']
     )
-    start = time.time()
-    df.to_sql('test_data', engine, if_exists='append')
-    end = time.time()
-    print("DataFrame.to_sql() time: {}".format(end - start))
 
     print('-'*20)
 
     start = time.time()
-    result = load_csv(df)
+    result = pg_handler.dataframe_to_sql(df, 'test_pg')
     end = time.time()
     elapsed_time = end - start
     print("psycopg2 copy_expert() time: {} Rows: {}".format(elapsed_time, result))
