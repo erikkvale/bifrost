a3b8042cd70b9c6ce8894b8d82cb0c1a1cb744b7
Erik Kvale <eirikval@gmail.com>
2018-09-01 12:26:55 -0500

    Added tests for sub classes, tests passed

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -7,3 +7,34 @@ class TestDbHandler:
     def test_cannot_instantiate_abc(self):
         with pytest.raises(TypeError):
             dbh = DbHandler()
+
+    def test_fulfilled_interface_of_subclass(self):
+        _conn_str = "Some database connection string"
+
+        class SubDbHandler(DbHandler):
+            def __init__(self, conn_str):
+                pass
+            def connection(self):
+                pass
+            def read(self):
+                pass
+            def write(self):
+                pass
+
+        sub_db_handler = SubDbHandler(_conn_str)
+
+    def test_un_fulfilled_interface_of_subclass(self):
+        _conn_str = "Some database connection string"
+
+        class SubDbHandler(DbHandler):
+            def __init__(self, conn_str):
+                pass
+            # def connection(self):
+            #     pass
+            def read(self):
+                pass
+            def write(self):
+                pass
+
+        with pytest.raises(TypeError):
+            sub_db_handler = SubDbHandler(_conn_str)
