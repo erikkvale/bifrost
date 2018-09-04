2d88e8db4cfeeac54e97c09d28f621a1119d4316
Erik Kvale <eirikval@gmail.com>
2018-09-03 22:44:48 -0500

    General sql handler

--- dbhandlers/postgresql.py
+++ dbhandlers/postgresql.py
@@ -1,39 +1,47 @@
 from .interface import DbHandler
 from collections import OrderedDict
 from sqlalchemy import create_engine
+from psycopg2 import sql
+import pandas
+import io
 
 
-class PostgresHandler(DbHandler):
+class SqlDbHandler:
 
     def __init__(self, conn_str):
         self.conn_str = conn_str
 
     @property
-    def connection(self):
-        """
-        Returns the underlying raw DBAPI connection
-        from the SQLAlchemy engine
-        """
-        engine = create_engine(self.conn_str)
-        if self._check_connection(engine):
-            return engine.raw_connection()
+    def engine(self):
+        """Returns the SQLAlchemy engine"""
+        return create_engine(self.conn_str)
 
     @property
-    def engine(self):
-        engine = create_engine(self.conn_str)
-        if self._check_connection(engine):
-            return engine
+    def connection(self):
+        """Returns the underlying DBAPI raw connection"""
+        return self.engine.raw_connection()
 
-    def read(self):
+    def read_sql(self, *args, **kwargs):
+        """Reads SQL data into Pandas DataFrame"""
+        return pandas.read_sql(*args, **kwargs)
+
+    def to_sql(self, dataframe, table,
+               bulk_load=True, *args, **kwargs):
+        """Loads a Pandas DataFrame to SQL with optional bulk insert"""
+        str_buff = io.StringIO()
+        dataframe.to_csv(str_buff)
+        table_composable = sql.Identifier(table)
+        sql_query = (sql.SQL("COPY {} FROM STDIN WITH CSV HEADER;").
+                     format(table_composable))
+        cursor = self.connection.cursor()
+        cursor.copy_expert(
+            file=str_buff.seek(0),
+            sql=sql_query
+        )
+        self.connection.commit()
+
+    def __str__(self):
         pass
 
-    def write(self):
+    def __repr__(self):
         pass
-
-    def _check_connection(self, engine):
-        try:
-            conn = engine.connect()
-            conn.close()
-        except Exception:
-            raise
-        return True
--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -1,40 +1,6 @@
-import pytest
-from sqlalchemy.exc import ArgumentError, OperationalError
-from sqlalchemy.pool import _ConnectionFairy
-
-from ..dbhandlers.postgresql import PostgresHandler
 
 
-class TestPostgresHandler:
 
-    @pytest.fixture(autouse=True)
-    def postgres_handler(self):
-        self.CONN_STR = "postgresql+psycopg2://postgres:Gunnar14@localhost:5432/test"
-        self.pg_handler = PostgresHandler(self.CONN_STR)
-
-    def test_initialization_sets_db_connection(self):
-        assert self.pg_handler.conn_str == self.CONN_STR
-
-    def test_check_connection_with_improperly_formatted_conn_str(self):
-        bad_conn_str = "Bad DSN connection string"
-        bad_pg_handle = PostgresHandler(bad_conn_str)
-        with pytest.raises(ArgumentError):
-            bad_pg_handle.connection
-
-    def test_check_connection_with_incorrect_password(self):
-        bad_pass_conn_str = "postgresql+psycopg2://postgres:Derpy@localhost:5432/test"
-        bad_pass_pg_handle = PostgresHandler(bad_pass_conn_str)
-        with pytest.raises(OperationalError):
-            bad_pass_pg_handle.connection
-
-    def test_db_connection_is_of_correct_type(self):
-        assert isinstance(self.pg_handler.connection, _ConnectionFairy)
-
-    def test_db_engine_is_of_correct_type(self):
-        assert isinstance(self.pg_handler.engine, type(self.pg_handler.engine))
-        raise pytest.fail("Fix the type comparison of the 2nd arg")
-
-    
 
 
 
