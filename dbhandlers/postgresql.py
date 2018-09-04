107ee8fb134e787a055b0f4f2cdf2609dd967104
Erik Kvale <eirikval@gmail.com>
2018-09-04 13:04:39 -0500

    Split basic functionality into functions before encapsulating in a class

--- dbhandlers/postgresql.py
+++ dbhandlers/postgresql.py
@@ -1,47 +1,84 @@
-from .interface import DbHandler
-from collections import OrderedDict
-from sqlalchemy import create_engine
-from psycopg2 import sql
 import pandas
+from sqlalchemy.exc import ProgrammingError
+from pandas.io.sql import get_schema
+from sqlalchemy import create_engine
+from psycopg2 import sql, DataError
 import io
 
 
-class SqlDbHandler:
+def initialize_engine(conn_str):
+    """
+    Creates, tests, and returns
+    a new SQLAlchemy engine object
+    """
+    engine = create_engine(conn_str)
+    try:
+        conn = engine.connect()
+        conn.close()
+    except Exception:
+        raise
+    return engine
 
-    def __init__(self, conn_str):
-        self.conn_str = conn_str
 
-    @property
-    def engine(self):
-        """Returns the SQLAlchemy engine"""
-        return create_engine(self.conn_str)
+def convert_dataframe(dataframe, *args, **kwargs):
+    """
+    Converts dataframe to an in-memory text
+    object and returns said file-like object
+    """
+    csv_buffer = io.StringIO()
+    dataframe.to_csv(csv_buffer, *args, **kwargs)
+    csv_buffer.seek(0)
+    return csv_buffer
 
-    @property
-    def connection(self):
-        """Returns the underlying DBAPI raw connection"""
-        return self.engine.raw_connection()
+def create_sql_table(dataframe, table_name, engine):
+    schema = get_schema(dataframe, table_name)
+    engine.execute(schema)
+    return None
 
-    def read_sql(self, *args, **kwargs):
-        """Reads SQL data into Pandas DataFrame"""
-        return pandas.read_sql(*args, **kwargs)
+def build_sql_query(table_name):
+    """
+    Creates and returns the PostgreSQL-specific
+    SQL query as a Composable type, to safely
+    parametrize and format the query
+    """
+    table_composable = sql.Identifier(table_name)
+    return sql.SQL(
+        "COPY {} FROM STDIN WITH CSV HEADER;"
+    ).format(table_composable)
 
-    def to_sql(self, dataframe, table,
-               bulk_load=True, *args, **kwargs):
-        """Loads a Pandas DataFrame to SQL with optional bulk insert"""
-        str_buff = io.StringIO()
-        dataframe.to_csv(str_buff)
-        table_composable = sql.Identifier(table)
-        sql_query = (sql.SQL("COPY {} FROM STDIN WITH CSV HEADER;").
-                     format(table_composable))
-        cursor = self.connection.cursor()
-        cursor.copy_expert(
-            file=str_buff.seek(0),
-            sql=sql_query
-        )
-        self.connection.commit()
 
-    def __str__(self):
-        pass
+def bulk_load(file, sql_query, engine):
+    raw_con = engine.raw_connection()
+    cursor = raw_con.cursor()
+    cursor.copy_expert(
+        file=file_obj,
+        sql=sql_query
+    )
+    raw_con.commit()
+    return None
 
-    def __repr__(self):
-        pass
+
+if __name__ == '__main__':
+    df = pandas.DataFrame(
+        data={
+            'col_1': [1, 2, 3],
+            'col_2': [3, 4, 5]
+        }
+    )
+    conn_str = "postgresql+psycopg2://postgres:Gunnar14@localhost:5432/test"
+    sql_table_name = "testing"
+
+    # Execution order
+    engine = initialize_engine(conn_str)
+    file_obj = convert_dataframe(df, index=False)
+    sql_query = build_sql_query(sql_table_name)
+
+    # Load data
+    try:
+        bulk_load(file_obj, sql_query, engine)
+    except DataError:
+        print(file_obj.getvalue())
+
+
+
+
