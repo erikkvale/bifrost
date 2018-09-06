dac3e430a40fd46620f1296802c63e0cbc96fab2
Erik Kvale <eirikval@gmail.com>
2018-09-05 21:53:05 -0500

    Added tests for initialize engine function

--- tests/test_postgresql.py
+++ tests/test_postgresql.py
@@ -1,3 +1,21 @@
+import pytest
+from ..dbhandlers.postgresql import (
+    initialize_engine,
+    DataframeLoader
+)
+from sqlalchemy.exc import OperationalError
+
+
+def test_initialize_engine_no_error():
+    good_conn_str = 'postgresql+psycopg2://postgres:Gunnar14@localhost/test'
+    initialize_engine(good_conn_str)
+
+
+def test_intialize_engine_raises_error():
+    bad_conn_str = 'postgresql+psycopg2://postgres:Gunnar14@localhost/tes'
+    with pytest.raises(OperationalError):
+        initialize_engine(bad_conn_str)
+
 
 
 
